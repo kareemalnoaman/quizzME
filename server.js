66d8fa5ac5f470222b6eabd1c7e3faa32bf3e9ce
Kareem Alnoaman <abdul.alnoaman@gmail.com>
2024-07-28 12:30:12 -0500

    pages updates

--- server.js
+++ server.js
@@ -4,65 +4,66 @@ const path = require('path')
 const app = express()
 const database = require("./models/database")
 const helpers = require('./helpers/functions')
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { error } = require('console')
 
 app.engine('hbs', engine({
     extname: 'hbs',
     defaultLayout: '',
     layoutsDir: '',
     partialsDir: path.join(__dirname, 'views/partials')
-  }))
+}))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.use(require("cookie-parser")())
 app.use(express.static('public'))
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }))
 app.use(express.json());
 
 app.use((req, res, next) => {
-    database.getConnection(function(err, connection) {
-      if (err) {
-        console.log(err)
-        res.render('404')
-      } else {
-        req.db = connection
-        next()
-      }
+    database.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err)
+            res.render('404')
+        } else {
+            req.db = connection
+            next()
+        }
     })
-  })
+})
 
 app.use((req, res, next) => {
 
-     if(req.cookies?.user) {
+    if (req.cookies?.user) {
         const userToken = req.cookies.user
         try {
             const user = jwt.verify(userToken, process.env.COOKIES_ENCRYPTION_KEY)
             req.user = user
-        } catch(err) {
+        } catch (err) {
             res.clearCookie('user')
         }
     }
     next()
 })
 
-app.get(['/help', '/quizzme'], (req, res, next) => {
-    if(!req.user) {
+app.get(['/help'], (req, res, next) => {
+    if (!req.user) {
         res.redirect('/login')
     } else {
         next()
     }
 })
 
-app.get('/', async (req,res) => {
+app.get('/', async (req, res) => {
     res.render('index', {
         user: req.user
     })
-   
+
 })
 
 app.get('/login', (req, res) => {
@@ -107,35 +108,35 @@ app.get('/admin/questions', (req, res) => {
 })
 
 app.post('/signup', async (req, res) => {
-    const {firstName, lastName, email, password} = req.body
-    if(
+    const { firstName, lastName, email, password } = req.body
+    if (
         helpers.isName(firstName + lastName) &&
         helpers.isEmail(email) &&
         helpers.isPassword(password)
     ) {
         try {
-        const salt = bcrypt.genSaltSync(parseInt(process.env.BYCRYPT_SALT_ROUNDS))
-        const hash = bcrypt.hashSync(password, salt);
-        const query = 'INSERT INTO Users (first_name,last_name,email,password) VALUES(?,?,?,?)'
-        const values = [firstName,lastName,email,hash]
-        req.db.query(query,values,(err, results) => {
-            if(err) {
-                console.log(err)
-                res.status(400).send('already exists')
-            } else {
-                const token = jwt.sign({
-                    id: results.insertId,
-                    firstName, lastName
-                }, process.env.COOKIES_ENCRYPTION_KEY)
-                res.cookie('user', token, {
-                    expires: new Date(Date.now() + 7 * 24 * 3600000),
-                    httpOnly: true
-                  })
-                res.status(200).send('200 ok')
-            }
-        })
-      
-        } catch(err) {
+            const salt = bcrypt.genSaltSync(parseInt(process.env.BYCRYPT_SALT_ROUNDS))
+            const hash = bcrypt.hashSync(password, salt);
+            const query = 'INSERT INTO Users (first_name,last_name,email,password) VALUES(?,?,?,?)'
+            const values = [firstName, lastName, email, hash]
+            req.db.query(query, values, (err, results) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send('already exists')
+                } else {
+                    const token = jwt.sign({
+                        id: results.insertId,
+                        firstName, lastName
+                    }, process.env.COOKIES_ENCRYPTION_KEY)
+                    res.cookie('user', token, {
+                        expires: new Date(Date.now() + 7 * 24 * 3600000),
+                        httpOnly: true
+                    })
+                    res.status(200).send('200 ok')
+                }
+            })
+
+        } catch (err) {
             console.log(err)
             res.status(400).send('Server error')
         }
@@ -145,38 +146,38 @@ app.post('/signup', async (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const {email, password} = req.body
-    if(
+    const { email, password } = req.body
+    if (
         helpers.isEmail(email) &&
         helpers.isPassword(password)
     ) {
         const query = 'SELECT * FROM Users where email = ?'
         const values = [email]
         req.db.query(query, values, (err, results) => {
-            if(err) {
+            if (err) {
                 res.status(400).send('Error loggin')
-            } else if(results.length === 0) {
+            } else if (results.length === 0) {
                 res.status(401).send('Invalid login credentials')
             } else {
                 const valid = bcrypt.compareSync(password, results[0].password)
 
-                if(valid) {
+                if (valid) {
                     const token = jwt.sign({
                         id: results[0].user_id,
                         firstName: results[0].first_name,
                         lastName: results[0].last_name
-                    },process.env.COOKIES_ENCRYPTION_KEY)
+                    }, process.env.COOKIES_ENCRYPTION_KEY)
                     res.cookie('user', token, {
                         expires: new Date(Date.now() + 7 * 24 * 3600000),
                         httpOnly: true
-                      })
+                    })
                     res.status(200).send('200 ok')
                 } else {
-                    res.status(400).send('Invalid login information') 
+                    res.status(400).send('Invalid login information')
                 }
             }
         })
-        
+
     } else {
         res.status(400).send('Invalid input')
     }
@@ -189,12 +190,12 @@ app.post('/logout', (req, res) => {
 
 app.post('/admin/questions/add-question', (req, res) => {
     console.log(req.body)
-    const {question, category, options, answer} = req.body
+    const { question, category, options, answer } = req.body
     const query = 'INSERT INTO Questions (question_subject, question_caregory) VALUES(?,?)'
     const values = [question, category]
     req.db.beginTransaction((err) => {
-        req.db.query('INSERT INTO Questions(question_subject, question_category) VALUES(?,?)',values, (err, results) => {
-            if(err) {
+        req.db.query('INSERT INTO Questions(question_subject, question_category) VALUES(?,?)', values, (err, results) => {
+            if (err) {
                 console.log(err)
                 res.status(400).send('Error adding question')
             } else {
@@ -205,7 +206,7 @@ app.post('/admin/questions/add-question', (req, res) => {
                 })
 
                 req.db.query('INSERT INTO question_options(question_id, option_text) values ?', [optionValues], (err, op_results) => {
-                    if(err) {
+                    if (err) {
                         console.log(err)
                         res.status(400).send('Error adding option')
                     } else {
@@ -227,7 +228,8 @@ app.post('/admin/questions/add-question', (req, res) => {
             }
         })
     })
-   
+
+
     // req.db.query(query, values, (err, results) => {
     //     if(err) {
     //         console.log(err)
@@ -238,4 +240,7 @@ app.post('/admin/questions/add-question', (req, res) => {
     // })
 })
 
+app.get("/price", (req, res) => {
+    res.render("price");
+})
 app.listen(3000)
